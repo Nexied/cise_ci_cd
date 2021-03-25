8e467fb4685e40d50c06a9f9d438f46382ffb2f4
Deni <msc0848@autuni.ac.nz>
2021-03-25 20:50:55 +1300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+importReact, { Component } from 'react';
+classAppextendsComponent{
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  makeIncrementer = amount=>()=>
+  this.setState(prevState=>({
+    count: prevState.count + amount,
+  }));
 
-export default App;
+  increment = this.makeIncrementer(1);
+
+  render() {
+    return(
+      <div>
+        <p>Count: {this.state.count}</p>
+        <button className="increment"onClick={this.increment}>Increment
+        count</button>
+      </div>
+      )
+    }
+  }
+  exportdefaultApp;
